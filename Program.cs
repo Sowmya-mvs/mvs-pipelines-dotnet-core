c37207fee0084e02d520c7e18bb52414f69a2df6
Sowmyashree M V <46842742+Sowmya-mvs@users.noreply.github.com>
2023-04-12 14:24:49 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace pipelines_dotnet_core
     {
         public static void Main(string[] args)
         {
+           String pwd='Temp1234';
             CreateHostBuilder(args).Build().Run();
         }
 
